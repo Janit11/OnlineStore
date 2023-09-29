ab345734b979e0d300d2615063fc9fe6b0d83122
Janit Kumar <kjanit15@gmail.com>
2023-09-29 11:38:31 -0700

    Added tailwind css, create header files,cart menu

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+//we use tailwind for styling and fast implementation
