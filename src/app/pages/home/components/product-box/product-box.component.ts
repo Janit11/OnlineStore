1ecf593a7b2dbcc182635bdcaab6a6ede51092c7
Janit Kumar <kjanit15@gmail.com>
2023-10-07 11:45:45 -0700

    implement cart logic, add, remove items.

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Product } from 'src/app/models/product.models';
 
 @Component({
@@ -8,13 +8,23 @@ import { Product } from 'src/app/models/product.models';
 export class ProductBoxComponent implements OnInit {
 
   @Input() fullWidthMode = false;
-  product: Product | undefined;
+  product: Product | undefined ={
+    id: 1,
+    title: 'hoodies',
+    price: 150,
+    category: 'clothes',
+    description: 'Description',
+    image: 'https://via.placeholder.com/150'
+  };
+
+  @Output() addToCart = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   onAddtoCart(): void{
-
+    this.addToCart.emit(this.product);
   }
 }
